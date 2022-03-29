c27b2a1a3e38139c07db3237198c102121505b75
elainezhang <elainezxff@gmail.com>
2022-03-29 18:05:37 +0800

    debugging

--- Assignment 3/Coding_assignment_3/S3006_Asm3_1155141566.R
+++ Assignment 3/Coding_assignment_3/S3006_Asm3_1155141566.R
@@ -61,26 +61,58 @@ rDirichlet <- function(alpha_vec){
 
 
 N = 5000 # iteration number
-pi_t = rep(NA, 1000*3*N) # to store pi_1, pi_2, and pi_3 for each iteration
-theta_t = rep(NA, 3*3*N) # to store theta for each iteration
-z_t = rep(NA, N, 1000) # to store Z for each iteration
-alpha_t = rep(NA, N, 3) # alpha values for Dirichlet Distribution
+pi_t = array(NA, dim = c(1000, 3, N) ) # to store pi_1, pi_2, and pi_3 for each iteration
+theta_t = array(NA, dim = c(3, 3, N) ) # to store theta for each iteration
+z_t = matrix(NA, N, 1000) # to store Z for each iteration
+alpha_t = matrix(NA, N, 3) # alpha values for Dirichlet Distribution
 
+set.seed(3006)
 
 for (i in 1:N) {
   if(i==1){
     # initialization
-    alpha_t[1, ] = rep(2, 3)
-    for (j in 1:N) {
-      pi_t[j, , 1] = rDirichlet(alpha_t[1, ])
+    alpha_t[1,] = rep(2, 1*3)
+    for (j in 1:1000) {
+      pi_t[j, , 1] = rDirichlet(alpha_t[1,])
     }
-    theta_t[ , , 1] = rep(1, 3, 3)
-    zi_1 = pi_t[ , 1, 1] * 
+    
+    theta_t[ , , 1] = matrix(1, 3, 3)
+    
+    zi_1 = pi_t[, 1, 1] * theta_t[1, 1, 1] *  theta_t[2, 1, 1] *  theta_t[3, 1, 1]
+    zi_2 = pi_t[, 2, 1] * theta_t[1, 2, 1] *  theta_t[2, 2, 1] *  theta_t[3, 2, 1]
+    zi_3 = pi_t[, 3, 1] * theta_t[1, 3, 1] *  theta_t[2, 3, 1] *  theta_t[3, 3, 1]
+    prob_1 = zi_1 / (zi_1 + zi_2 + zi_3)
+    prob_2 = zi_2 / (zi_1 + zi_2 + zi_3)
+    prob_3 = zi_3 / (zi_1 + zi_2 + zi_3)
+    for (j in 1:1000) {
+      z_t[1, j] = sample(1:3, 1, prob = c(prob_1[j], prob_2[j], prob_3[j]), replace = TRUE)
+    }
     
   }
   else{
+    alpha_t[i, ] = alpha_t[i-1, ] + c(sum(z_t[i-1, ] == 1), sum(z_t[i-1, ] == 2), sum(z_t[i-1, ] == 3))
+    for (j in 1:1000) {
+      pi_t[j, , i] = rDirichlet(alpha_t[i, ])
+    }
+    
+    for (j in 1:3) {
+      for (k in 1:3) {
+        theta_t[j, k, i] = rbinom(1, 10*j, theta_t[j, k, i-1])
+      }
+    }
+    
+    zi_1 = pi_t[ , 1, i] * theta_t[1, 1, i] *  theta_t[2, 1, i] *  theta_t[3, 1, i]
+    zi_2 = pi_t[ , 2, i] * theta_t[1, 2, i] *  theta_t[2, 2, i] *  theta_t[3, 2, i]
+    zi_3 = pi_t[ , 3, i] * theta_t[1, 3, i] *  theta_t[2, 3, i] *  theta_t[3, 3, i]
+    prob_1 = zi_1 / (zi_1 + zi_2 + zi_3)
+    prob_2 = zi_2 / (zi_1 + zi_2 + zi_3)
+    prob_3 = zi_3 / (zi_1 + zi_2 + zi_3)
+    for (j in 1:1000) {
+      z_t[i, j] = sample(1:3, 1, prob = c(prob_1[j], prob_2[j], prob_3[j]), replace = TRUE)
+    }
     
   }
+
 }
 
 
