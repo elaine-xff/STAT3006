94cb4df7129cf9dbe47cb21d88fca37a7685229c
elainezhang <elainezxff@gmail.com>
2022-04-07 00:43:48 +0800

    finished hw3

--- Assignment 3/Coding_assignment_3/S3006_Asm3_1155141566.R
+++ Assignment 3/Coding_assignment_3/S3006_Asm3_1155141566.R
@@ -42,7 +42,6 @@ estimated_lambda = mean(lambda_t[(B+1):N])
 
 
 ## Question 2: Gibbs Sampler for clustering
-
 rm(list = ls())
 
 # Read Data
@@ -61,41 +60,45 @@ rDirichlet <- function(alpha_vec){
 
 
 N = 5000 # iteration number
-#pi_t = array(NA, dim = c(1000, 3, N) ) # pi_1, pi_2, and pi_3 for each iteration
 alpha_t = matrix(NA, N, 3) # alpha values for Dirichlet Distribution
 pi_t = matrix(NA, N, 3)
 
-beta_t = matrix(NA, N, 2) # parameters in beta distribution
 theta_t = matrix(NA, N, 9) # theta_[11, 12, 13, ..., 31, 32, 33] for each iteration
 
 z_t = matrix(NA, N, 1000) # to store Z for each iteration
 
-
 set.seed(3006)
 
-for (i in 1:N) {
-  if(i==1){
-    # initialization
-    alpha_t[1,] = rep(2, 1*3)
-    pi_t[1, ] = rDirichlet(alpha_t[1,])
-    beta_t[1, ] = c(1, 1)
-    theta_t[1, ] = rbeta(9, beta_t[1,1], beta_t[1,2])
-    
-    binom_k_1 = pi_t[1, 1] * dbinom(Q2Data[,1], 10 * 1, theta_t[1,1]) * dbinom(Q2Data[,2], 10 * 2, theta_t[4,1]) * dbinom(Q2Data[,3], 10 * 3, theta_t[7,1])
-    binom_k_2 = pi_t[2, 1] * dbinom(Q2Data[,1], 10 * 1, theta_t[2,1]) * dbinom(Q2Data[,2], 10 * 2, theta_t[5,1]) * dbinom(Q2Data[,3], 10 * 3, theta_t[8,1])
-    binom_k_3 = pi_t[3, 1] * dbinom(Q2Data[,1], 10 * 1, theta_t[3,1]) * dbinom(Q2Data[,2], 10 * 2, theta_t[6,1]) * dbinom(Q2Data[,3], 10 * 3, theta_t[9,1])
-    
-    prob_1 = binom_k_1 / (binom_k_1 + binom_k_2 + binom_k_3)
-    prob_2 = binom_k_2 / (binom_k_1 + binom_k_2 + binom_k_3)
-    prob_3 = binom_k_3 / (binom_k_1 + binom_k_2 + binom_k_3)
-    
-    z_t[1, ] = sample(1:3, 1, prob = c(prob_1, prob_2, prob_3), replace = TRUE)
-#    for (j in 1:1000) {
-#      z_t[1, j] = sample(1:3, 1, prob = c(prob_1[j], prob_2[j], prob_3[j]), replace = TRUE)
-#    }
-    
-  }
-  else{
+# initialization
+alpha_t[1,] = rep(2, 1*3)
+pi_t[1, ] = rDirichlet(alpha_t[1,])
+#beta_t[1, ] = c(1, 1)
+theta_t[1, ] = rbeta(9, 1, 1)
+  
+binom_k_1 = pi_t[1, 1] * dbinom(Q2Data[,1], 10 * 1, theta_t[1,1]) * dbinom(Q2Data[,2], 10 * 2, theta_t[1,4]) * dbinom(Q2Data[,3], 10 * 3, theta_t[1,7])
+binom_k_2 = pi_t[1, 2] * dbinom(Q2Data[,1], 10 * 1, theta_t[1,2]) * dbinom(Q2Data[,2], 10 * 2, theta_t[1,5]) * dbinom(Q2Data[,3], 10 * 3, theta_t[1,8])
+binom_k_3 = pi_t[1, 3] * dbinom(Q2Data[,1], 10 * 1, theta_t[1,3]) * dbinom(Q2Data[,2], 10 * 2, theta_t[1,6]) * dbinom(Q2Data[,3], 10 * 3, theta_t[1,9])
+  
+prob_1 = binom_k_1 / (binom_k_1 + binom_k_2 + binom_k_3)
+prob_2 = binom_k_2 / (binom_k_1 + binom_k_2 + binom_k_3)
+prob_3 = binom_k_3 / (binom_k_1 + binom_k_2 + binom_k_3)
+  
+z_df <- data.frame (p1 = prob_1,
+                    p2 = prob_2,
+                    p3 = prob_3,
+                    cluster = NA
+)
+
+for (i in 1:1000) {
+  z_df[i, 4] = sample(1:3, 1, prob=c(z_df[i,1],z_df[i,2],z_df[i,3]), replace=TRUE)
+}
+
+z_t[1, ] = z_df$cluster
+
+
+
+
+for (i in 2:N) {
     alpha_t[i, ] = alpha_t[i-1, ] + c(sum(z_t[i-1, ] == 1), sum(z_t[i-1, ] == 2), sum(z_t[i-1, ] == 3))
     pi_t[i, ] = rDirichlet(alpha_t[i, ])
     
@@ -103,34 +106,41 @@ for (i in 1:N) {
     Data = cbind(Q2Data, z)
     
     for (j in 1:3) {
+      Data_to_compute = Data[, c(j, 4)]
       for (k in 1:3) {
-        
-        theta_t[j, k, i] = rbeta(1, 1+sum_x, 1+10*j*sum(z_t[i-1, ] == k) - sum_x)
+        sum_x = sum(Data_to_compute[which(Data_to_compute$z == k), 1])
+        theta_t[i, (j-1)*3 + k] = rbeta(1, 1+sum_x, 1+10*j*sum(z == k) - sum_x)
       }
     }
     
-    binom_k_1 = pi_t[1, i] * dbinom(Q2Data[,1], 10 * 1, theta_t[1,i]) * dbinom(Q2Data[,2], 10 * 2, theta_t[4,i]) * dbinom(Q2Data[,3], 10 * 3, theta_t[7,i])
-    binom_k_2 = pi_t[2, i] * dbinom(Q2Data[,1], 10 * 1, theta_t[2,i]) * dbinom(Q2Data[,2], 10 * 2, theta_t[5,i]) * dbinom(Q2Data[,3], 10 * 3, theta_t[8,i])
-    binom_k_3 = pi_t[3, i] * dbinom(Q2Data[,1], 10 * 1, theta_t[3,i]) * dbinom(Q2Data[,2], 10 * 2, theta_t[6,i]) * dbinom(Q2Data[,3], 10 * 3, theta_t[9,i])
+    binom_k_1 = pi_t[i, 1] * dbinom(Q2Data[,1], 10 * 1, theta_t[i,1]) * dbinom(Q2Data[,2], 10 * 2, theta_t[i,4]) * dbinom(Q2Data[,3], 10 * 3, theta_t[i,7])
+    binom_k_2 = pi_t[i, 2] * dbinom(Q2Data[,1], 10 * 1, theta_t[i,2]) * dbinom(Q2Data[,2], 10 * 2, theta_t[i,5]) * dbinom(Q2Data[,3], 10 * 3, theta_t[i,8])
+    binom_k_3 = pi_t[i, 3] * dbinom(Q2Data[,1], 10 * 1, theta_t[i,3]) * dbinom(Q2Data[,2], 10 * 2, theta_t[i,6]) * dbinom(Q2Data[,3], 10 * 3, theta_t[i,9])
     
     prob_1 = binom_k_1 / (binom_k_1 + binom_k_2 + binom_k_3)
     prob_2 = binom_k_2 / (binom_k_1 + binom_k_2 + binom_k_3)
     prob_3 = binom_k_3 / (binom_k_1 + binom_k_2 + binom_k_3)
     
-    z_t[i, ] = sample(1:3, 1, prob = c(prob_1, prob_2, prob_3), replace = TRUE)
-#    for (j in 1:1000) {
-#      z_t[i, j] = sample(1:3, 1, prob = c(prob_1[j], prob_2[j], prob_3[j]), replace = TRUE)
-#    }
+    z_df <- data.frame (p1 = prob_1,
+                        p2 = prob_2,
+                        p3 = prob_3,
+                        cluster = NA
+    )
     
-  }
+    for (p in 1:1000) {
+      z_df[p, 4] = sample(1:3, 1, prob=c(z_df[p,1],z_df[p,2],z_df[p,3]), replace=TRUE)
+    }
+    
+    z_t[i, ] = z_df$cluster
 
 }
 
 B = 3000 # burn-in period
+pi_used = pi_t[(B+1):N, ]
+estimated_pi = apply(pi_used, 2, mean)
 
-estimated_pi = mean(pi_t[(B+1:N), ])
-
-estimated_theta = mean(theta_t[(B+1:N), ])
+theta_used = theta_t[(B+1):N, ]
+estimated_theta = apply(theta_used, 2, mean)
 
 estimated_z = matrix(NA, 1000, 1)
 for(j in 1:1000){
@@ -143,11 +153,11 @@ for(j in 1:1000){
 # plot
 dev.off()
 
-plot(theta_t[1,1,], ylim = c(0.75,0.85))
+plot(theta_t[, 1], type = "l")
 
-plot(z_t[, 1])
+plot(z_t[, 1], type = "l")
 
-plot(pi_t[,,1])
+plot(pi_t[ ,1], type = "l")
 
 
 
@@ -157,99 +167,7 @@ rm(list = ls())
 set.seed(3006)
 
 N = 10000 #iteration number
-p_1_t = matrix(NA, N, 4) # prob. for stage 1
-p_2_t = matrix(NA, N, 4) # prob. for stage 2
-y_t = matrix(NA, N, 4) # col 1 for y11, 2 for y12, 3 for y22, 4 for y24
-
-#function used to sample Dirichlet distributed r.v.
-rDirichlet <- function(alpha_vec){
-  num <- length(alpha_vec)
-  temp <- NULL
-  for(i in 1:num){
-    temp <- c(temp, rgamma(1, shape = alpha_vec[i], rate = 1))
-  }
-  return(temp/sum(temp))
-} 
-
-# initialization
-p_1_t[1, ] = rDirichlet(c(2,2,2,2))
-p_2_t[1, ] = p_1_t[1, ]
-y_t[1, ] = c(100-22-31-20, 20, 18, 100-28-26-18)
-
-# Hybrid Gibbs Sampler
-accept_num = 0
-for (i in 2:N) {
-  # sample p's
-  p_1_t[i, ] = rDirichlet(p_1_t[i-1, ] + c(y_t[i-1, 1], y_t[i-1, 2], 22, 31))
-  p_2_t[i, ] = rDirichlet(p_2_t[i-1, ] + c(28, y_t[i-1, 3], 26, y_t[i-1, 4]))
-  
-  # sample unobserved y's
-  # sample yi2 (i = 1,2)
-  for (j in 2:3) {
-    # get a proposal distribution depends on its last iteration
-    if (y_t[i-1, j] == 15){
-      y_proposal = y_t[i-1, j] + 1
-    }
-    if (y_t[i-1, j] == 32){
-      y_proposal = y_t[i-1, j] - 1
-    }
-    if ( (y_t[i-1, j]>15) & (y_t[i-1, j]<32) ){
-      y_proposal = y_t[i-1, j] + sample(c(-1, 1), 1)
-    }
-    
-    if( j==2 ){
-      p_2 = p_1_t[i, 2]
-    }else{
-      p_2 = p_2_t[i, 2]
-    }
-    
-    r = min ( (p_2^(y_proposal)/factorial(y_proposal))/(p_2^(y_t[i-1, j])/factorial(y_t[i-1, j])), 1 )
-    
-    if(runif(1) < r){
-      y_t[i, j] = y_proposal # accept proposal
-      accept_num = accept_num + 1
-    }else{
-      y_t[i, j] = y_t[i-1, j] # reject proposal
-    }
-    
-  }
-  
-  y_t[i, 1] = 100 - 22 - 31 - y_t[i, 2]
-  y_t[i, 4] = 100 - 28 - 26 - y_t[i, 3]
-  
-}
-
-accept_ratio = accept_num / (2*N) # to check if the acceptance rate is good enough
-
-
-B = 5000 # burn-in period
-estimated_p_1 = rep(NA, 4)
-estimated_p_2 = rep(NA, 4)
-for (j in 1:4) {
-  estimated_p_1[j] = mean(p_1_t[(B+1):N, j])
-  estimated_p_2[j] = mean(p_2_t[(B+1):N, j])
-}
-
-
-## no run
-# plot
-dev.off()
-
-plot(theta_t[1,1,], ylim = c(0.75,0.85))
-
-plot(z_t[, 1])
-
-plot(pi_t[,,1])
-
-
-######################################################
-## new try
-
-rm(list = ls())
-set.seed(3006)
-
-N = 10000 #iteration number
-p_t = matrix(NA, N, 4) # prob. for stage 1
+p_t = matrix(NA, N, 4) # prob. for multi-nomial dist.
 y_t = matrix(NA, N, 4) # col 1 for y11, 2 for y12, 3 for y22, 4 for y24
 
 #function used to sample Dirichlet distributed r.v.
@@ -270,7 +188,7 @@ y_t[1, ] = c(100-22-31-20, 20, 18, 100-28-26-18)
 accept_num = 0
 for (i in 2:N) {
   # sample p's
-  p_t[i, ] = rDirichlet(p_t[i-1, ] + c(28, y_t[i-1, 2], 22, 31))
+  p_t[i, ] = rDirichlet(p_t[i-1, ] + c((28+y_t[i-1, 1])/2, (y_t[i-1, 2]+y_t[i-1, 3])/2, (22+26)/2, (31+y_t[i-1, 4])/2))
   
   # sample unobserved y's
   # sample yi2 (i = 1,2)
