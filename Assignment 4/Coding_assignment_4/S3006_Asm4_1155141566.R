b556da8a4ee71cb54d8cf26635d21de3ec8deb3c
elainezhang <elainezxff@gmail.com>
2022-04-11 23:30:47 +0800

    finished q2

--- Assignment 4/Coding_assignment_4/S3006_Asm4_1155141566.R
+++ Assignment 4/Coding_assignment_4/S3006_Asm4_1155141566.R
@@ -1,1 +1,42 @@
-##
+## Question 1:
+
+
+## Question 2: Database Access from R
+
+#install.packages("RMySQL")
+#install.packages("DBI")
+library("RMySQL")
+
+drv=dbDriver("MySQL")
+con=dbConnect(drv,user="student", password="HappyStudy2022",
+              dbname="Library", port=3306,
+              host="rds-mysql-statclass.czyn7pdbk60s.us-west-2.rds.amazonaws.com")
+
+dbListTables(con)
+dbReadTable(con,"Student", "Book", "Record")
+
+# (a) list the 'Book' table
+dbGetQuery(con,"SELECT * FROM Book;")
+
+# (b) find the ids and the entry years of students 
+#     who have borrowed book from natural science
+dbGetQuery(con, "SELECT Student.StudentID, EntryYear FROM Student, Record, Book
+    where Book.Classification = 'Natural Science'
+    And Record.BookNumber = Book.BookNumber
+    And Record.StudentID = Student.StudentID;")
+
+# (c) find the ids and majors of the students who have 
+#     borrowed book 008 and occupied it for more than 30 days
+dbGetQuery(con, "SELECT Student.StudentID, Major FROM Student, Record
+           where Record.BookNumber = '8'
+           And Record.StudentID = Student.StudentID
+           And TIMESTAMPDIFF(day, BorrowingTime, ReturnTime)>30;")
+        
+dbDisconnect(con)
+dbUnloadDriver(drv)
+
+
+## Question 3:
+
+
+
